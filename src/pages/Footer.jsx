9dc3bb7d17a4a36989a26c261ec2681cbcf6d0bd
Nelson <nelson.pena@chasacademy.se>
2025-04-12 07:25:36 +0200

    first 1

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,13 +2,27 @@ import "../pagesCSS/Footer.css";
 
 const Footer = () => (
   <footer className="footer-container">
-    <a href="/assets/images2/Magasin.pdf" download="Magasin.pdf">
+    <a
+      href="/assets/images2/Magasin.pdf"
+      download="Magasin.pdf"
+      type="application/pdf"
+    >
       Tidning
     </a>
-    <a href="https://www.linkedin.com/in/nelson-pe%C3%B1a-21881412a/" target="_blank" rel="noopener noreferrer">
+
+    <a
+      href="https://www.linkedin.com/in/nelson-pe%C3%B1a-21881412a/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       Linkedin
     </a>
-    <a href="/assets/images2/CV_Nelson_Pena.pdf" download="CV_Nelson_Peña.pdf">
+
+    <a
+      href="/assets/images2/CV_Nelson_Pena.pdf"
+      download="CV_Nelson_Pena.pdf"
+      type="application/pdf"
+    >
       Ladda ner CV
     </a>
   </footer>
